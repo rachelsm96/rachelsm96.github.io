8f8a453bafd50d74d8d00fb25872f1f16418f668
rachelsm96 <martinez19x96@gmail.com>
2020-04-27 21:38:07 -0500

    updated colors

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //javascirpt function will go in this file
 
 document.getElementById("github").addEventListener("mouseover", function () {
-    document.getElementById("body").style.backgroundColor = "green";
+    document.getElementById("body").style.backgroundColor = "limegreen";
     document.getElementById("jumbotron").style.color = "black";
     Array.from(document.getElementsByClassName("icon")).forEach(
         function (element, index, array) {
-            element.style.color = "yellow";
+            element.style.color = "black";
         }
     );
 });
 
-document.getElementById("linkedin").addEventListener("mouseover", function () {
-    document.getElementById("body").style.backgroundColor = "blue";
+document.getElementById("resume").addEventListener("mouseover", function () {
+    document.getElementById("body").style.backgroundColor = "orchid";
     document.getElementById("jumbotron").style.color = "black";
     Array.from(document.getElementsByClassName("icon")).forEach(
         function (element, index, array) {
@@ -32,7 +32,7 @@ document.getElementById("welcome").addEventListener("mouseover", function () {
 });
 
 
-document.getElementById("resume").addEventListener("mouseover", function () {
+document.getElementById("linkedin").addEventListener("mouseover", function () {
     document.getElementById("body").style.backgroundColor = "CornflowerBlue";
     document.getElementById("jumbotron").style.color = "white";
     Array.from(document.getElementsByClassName("icon")).forEach(
